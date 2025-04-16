6624c0580c5bbe6458a0d258f44d4bbeb6cce71a
AzureAD\ZachReeder <zreeder@kyledavidgroup.com>
2025-04-16 11:52:50 -0400

    Minor nitpick fix for public function names in database class.

--- KDG.Net.Database/interfaces/IDatabase.cs
+++ KDG.Net.Database/interfaces/IDatabase.cs
@@ -1,6 +1,10 @@
 namespace KDG.Database.Interfaces;
 public interface IDatabase<TConnection, TTransaction> {
     string ConnectionString { get; set; }
+    [Obsolete("Use WithConnection<A>(Func<TConnection, Task<A>> execute) instead.")]
     Task<A> withConnection<A>(Func<TConnection, Task<A>> execute);
+    Task<A> WithConnection<A>(Func<TConnection, Task<A>> execute);
+    [Obsolete("Use WithTransaction<A>(Func<TTransaction, Task<A>> execute) instead.")]
     Task<A> withTransaction<A>(Func<TTransaction, Task<A>> execute);
+    Task<A> WithTransaction<A>(Func<TTransaction, Task<A>> execute);
 }
